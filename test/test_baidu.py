a5663c6883ec958857d5d1211497539f29e70b91
oldcat0426 <marco_5212003@139.com>
2018-11-14 11:21:51 +0800

    newfile test_baidu

---
+++ test/test_baidu.py
@@ -0,0 +1,8 @@
+import os
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+URL = "https://www.baidu.com"
+base_path = os.path.dirname(os.path.abspath(__file__))
+print(base_path)
